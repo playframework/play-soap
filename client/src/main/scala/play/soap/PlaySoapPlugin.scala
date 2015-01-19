0e75ce99fb35ea7e07727eed4d17485fe0786531
James Roper <james@jazzy.id.au>
2015-01-19 15:58:39 +1100

    Ensured Play SOAP uses the async http client
    Also Introduced a plugin for managing the Apache CXF bus, both
    configuration and lifecycle.

--- client/src/main/scala/play/soap/PlaySoapPlugin.scala
+++ client/src/main/scala/play/soap/PlaySoapPlugin.scala
@@ -2,11 +2,17 @@ package play.soap
 
 import javax.xml.namespace.QName
 
+import org.apache.cxf.BusFactory
 import org.apache.cxf.interceptor.{LoggingOutInterceptor, LoggingInInterceptor}
+import org.apache.cxf.transport.ConduitInitiatorManager
+import org.apache.cxf.transport.http.asyncclient.{AsyncHTTPConduitFactory, AsyncHTTPConduit, AsyncHttpTransportFactory}
 import play.api._
 
 import scala.reflect.ClassTag
 
+/**
+ * Abstract plugin extended by all generated SOAP clients
+ */
 abstract class PlaySoapPlugin(app: Application) extends Plugin {
 
   private lazy val config = Configuration(app.configuration.underlying.getConfig("play.soap"))
@@ -31,7 +37,9 @@ abstract class PlaySoapPlugin(app: Application) extends Plugin {
   }
 
   protected def createPort[T](qname: QName, portName: String, defaultAddress: String)(implicit ct: ClassTag[T]): T = {
-    val factory = new PlayJaxWsProxyFactoryBean
+
+    val factory = createFactory
+
     if (shouldLog(qname)) {
       factory.getInInterceptors.add(new LoggingInInterceptor)
       factory.getOutInterceptors.add(new LoggingOutInterceptor)
@@ -41,10 +49,65 @@ abstract class PlaySoapPlugin(app: Application) extends Plugin {
       .flatMap(_.address)
       .getOrElse(defaultAddress)
     factory.setAddress(address)
-    factory.create().asInstanceOf[T]
+
+    val port = factory.create()
+
+    port.asInstanceOf[T]
   }
 
   private def shouldLog(qname: QName) = configuredPorts.get(qname).fold(debugLogDefault)(_.debugLog)
+
+  private def createFactory = {
+    app.plugin[ApacheCxfBusPlugin] match {
+      case Some(plugin) =>
+        val factory = new PlayJaxWsProxyFactoryBean
+        factory.setBus(plugin.bus)
+        factory
+      case None =>
+        throw new IllegalStateException("play.soap.ApacheCxfBusPlugin is not enabled!")
+    }
+  }
+}
+
+/**
+ * Configures and manages the lifecycle of an Apache CXF bus
+ */
+class ApacheCxfBusPlugin(app: Application) extends Plugin {
+
+  private lazy val asyncTransport = new AsyncHttpTransportFactory
+  private[soap] lazy val bus = {
+
+    val bus = BusFactory.newInstance.createBus
+
+    // Although Apache CXF will automatically select the async http transport conduit, we want to ensure that it will
+    // use ours no matter what, so we do that here.  Note - in future we could replace this with one based on WS.
+    val cim = bus.getExtension(classOf[ConduitInitiatorManager])
+    cim.registerConduitInitiator("http://cxf.apache.org/transports/http", asyncTransport)
+    cim.registerConduitInitiator("http://cxf.apache.org/transports/https", asyncTransport)
+    cim.registerConduitInitiator("http://cxf.apache.org/transports/http/configuration", asyncTransport)
+    cim.registerConduitInitiator("http://cxf.apache.org/transports/https/configuration", asyncTransport)
+
+    bus.setProperty(AsyncHTTPConduit.USE_ASYNC, java.lang.Boolean.TRUE)
+
+    bus
+  }
+
+  override def onStop() = {
+    bus.shutdown(true)
+
+    // The AsyncHttpTransportFactory holds an AsyncHTTPConduitFactory, which holds a client which holds threads. There
+    // is no way to shut this down without using reflection to get the conduit factory.
+    try {
+      val factoryField = classOf[AsyncHttpTransportFactory].getDeclaredField("factory")
+      factoryField.setAccessible(true)
+      val factory = factoryField.get(asyncTransport).asInstanceOf[AsyncHTTPConduitFactory]
+      factory.shutdown()
+    } catch {
+      // Ignore, just print the stack trace so we know something has gone wrong
+      case e: Exception => e.printStackTrace()
+    }
+  }
+
 }
 
 private case class PortConfig(namespace: QName, address: Option[String], debugLog: Boolean)
