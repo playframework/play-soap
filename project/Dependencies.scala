d54144a7566a91d11f653a8d9005102682644ebb
Scala Steward <me@scala-steward.org>
2020-05-20 22:37:56 +0200

    Update play, play-specs2 to 2.7.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val CXF  = "3.3.6"
-    val Play = "2.7.4"
+    val Play = "2.7.5"
   }
 
   val `play-client` = libraryDependencies ++= Seq(
