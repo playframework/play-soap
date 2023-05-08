262aaea86d3affe38d14bc9a7d3794e823ea1a64
Matthias Kurz <m.kurz@irregular.at>
2023-05-08 13:53:45 +0200

    Update copyright (again)

--- plugin/src/sbt-test/play-soap/incremental-compilation/project/Constants.scala
+++ plugin/src/sbt-test/play-soap/incremental-compilation/project/Constants.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
 object Constants {
   // scripted swallows double quotes, so can't use them in a set command, so need to refer to a constant if you want
