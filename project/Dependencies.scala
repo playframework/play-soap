c4afc37af71bacb70ada0089ebba91cbf65a20a0
Scala Steward <scala_steward@virtuslab.com>
2025-04-24 18:59:47 +0000

    junit-jupiter 1.21.0 (was 1.20.6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
     "org.apache.cxf"     % "cxf-rt-transports-http-hc5" % Versions.CXF         % Test,
     "com.typesafe.play" %% "play"                       % Versions.Play        % Test, // TODO: remove
     "net.aichler"        % "jupiter-interface"          % jupiterVersion.value % Test,
-    "org.testcontainers" % "junit-jupiter"              % "1.20.6"             % Test,
+    "org.testcontainers" % "junit-jupiter"              % "1.21.0"             % Test,
     "org.assertj"        % "assertj-core"               % "3.27.3"             % Test
   )
 
