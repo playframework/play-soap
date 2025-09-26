7e3a32370a93d83e16414e5c100a9e659fbc2e4b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-09-26 23:35:50 +0200

    assertj-core 3.27.6 (was 3.27.4) (#504)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
     "org.apache.cxf"     % "cxf-rt-transports-http-hc5" % Versions.CXF         % Test,
     "net.aichler"        % "jupiter-interface"          % jupiterVersion.value % Test,
     "org.testcontainers" % "junit-jupiter"              % "1.21.3"             % Test,
-    "org.assertj"        % "assertj-core"               % "3.27.4"             % Test
+    "org.assertj"        % "assertj-core"               % "3.27.6"             % Test
   )
 
   val `test-scala` = libraryDependencies ++= Seq(
